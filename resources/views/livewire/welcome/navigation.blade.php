e7af3f94e5cb3a5c52690528976e2623eb6f3200
TopherNeil <gomez.cneil@gmail.com>
2025-01-10 00:11:51 +0800

    Tweaked navigation

--- resources/views/livewire/welcome/navigation.blade.php
+++ resources/views/livewire/welcome/navigation.blade.php
@@ -1,4 +1,4 @@
-<nav class="-mx-3 flex flex-1 justify-end">
+<nav class="fixed top-10 right-10">
     @auth
         <a
             href="{{ url('/dashboard') }}"
@@ -7,20 +7,23 @@
             Dashboard
         </a>
     @else
-        <a
+        <x-button
             href="{{ route('login') }}"
-            class="rounded-md px-3 py-2 text-black ring-1 ring-transparent transition hover:text-black/70 focus:outline-none focus-visible:ring-[#FF2D20] dark:text-white dark:hover:text-white/80 dark:focus-visible:ring-white"
-        >
-            Log in
-        </a>
+            outline
+            secondary
+            label="Login"
+            class="m-2 text-black bg-slate-200 hover:text-white hover:bg-slate-700"
+        />
+            
 
         @if (Route::has('register'))
-            <a
+            <x-button
                 href="{{ route('register') }}"
-                class="rounded-md px-3 py-2 text-black ring-1 ring-transparent transition hover:text-black/70 focus:outline-none focus-visible:ring-[#FF2D20] dark:text-white dark:hover:text-white/80 dark:focus-visible:ring-white"
-            >
-                Register
-            </a>
+                flat
+                secondary
+                label="Register"
+                class="m-2 text-black bg-slate-200 hover:text-white hover:bg-slate-700"
+            />
         @endif
     @endauth
 </nav>
