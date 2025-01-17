1a7dc497c4b7ac87d12b94dd8e79d671bdef8c6f
TopherNeil <gomez.cneil@gmail.com>
2025-01-17 17:55:07 +0800

    Added Routing for Post, Authentication and Pages

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,60 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
-Route::get('/', function () {
-    return view('pages.welcome');
-});
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\PostController;
+use App\Http\Controllers\SearchController;
+use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\RegisterController;
+use App\Http\Controllers\NotificationController;
 
 Route::get('**', function () {
     return view('errors.404');
+});
+
+Route::middleware('guest')->group(function() {
+
+    // Authentication
+    Route::get('/login', [AuthController::class, 'showLoginForm'])->name('login');
+    Route::post('/login', [AuthController::class, 'login']);
+    Route::get('/register', [RegisterController::class, 'showRegisterForm'])->name('register');
+    Route::post('/register', [RegisterController::class, 'register']);
+
+    // Guest Home
+    Route::get('/', [PostController::class, 'index'])->name('home');
+});
+
+
+
+Route::middleware('auth')->group(function() {
+    
+    // Home
+    Route::get('/home', [PostController::class, 'index'])->name('home');
+    
+    // Post
+    Route::get('/post', [PostController::class, 'create'])->name('post');
+    Route::get('/post/{id}', [PostController::class, 'show']);
+    Route::get('/post/edit/{id}', [PostController::class, 'edit']);
+    Route::put('/post/edit/{id}', [PostController::class, 'update']);
+    Route::post('/post', [PostController::class, 'store']);
+    Route::delete('/post/delete/{id}', [PostController::class, 'destroy']);
+
+    // Search
+    Route::get('/search', [SearchController::class, 'index'])->name('search');
+
+    // Profile
+    Route::get('/profile', [ProfileController::class, 'index'])->name('profile');
+
+    // Notification
+    Route::get('/notification', [NotificationController::class, 'index'])->name('notification');
+    
+    // Logout
+    Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
+});
+
+
+
+Route::get('/clear-session', function() {
+    AuthController::clearMessage();
+    return redirect()->back();
 });
