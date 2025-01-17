2622a7be8220638ba5b9663a4502ac664e2ea3ee
TopherNeil <gomez.cneil@gmail.com>
2025-01-17 17:52:47 +0800

    Added Registration

---
+++ app/Http/Controllers/RegisterController.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+
+class RegisterController extends Controller
+{
+    public function showRegisterForm()
+    {
+        return view('pages.auth.register');
+    }
+
+    public function register(Request $request)
+    {
+        $form = $request->validate([
+            'username' => 'required|string|max:15',
+            'email' => 'required|email|unique:users,email|max:255',
+            'password' => 'required|string|min:8|confirmed'
+        ]);
+
+        try {
+            $form['name'] = 'Chon';
+            $form['password'] = Hash::make($form['password']);
+            $user = User::create($form);
+            Auth::login($user);
+            return redirect()->intended('home');
+            
+        } catch (\Exception $e) {
+            report($e);
+        }
+        
+    }
+}
