123faa663aeed1a5497be0ea1da9dad41e03f895
TopherNeil <gomez.cneil@gmail.com>
2025-01-17 17:54:10 +0800

    Tweaked layout selector

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -20,6 +20,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        Blade::component('layouts.app', 'layouts-app');
+        Blade::component('layouts.app', 'layout');
     }
 }
