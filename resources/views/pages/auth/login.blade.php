edab0e8cf7576765bfc561f8077b0d906cf47721
TopherNeil <gomez.cneil@gmail.com>
2025-01-18 14:52:28 +0800

    Enhanced UI/UX experience  on login and registration pages

--- resources/views/pages/auth/login.blade.php
+++ resources/views/pages/auth/login.blade.php
@@ -1,32 +1,38 @@
-<x-layout :title="'Login'" class="flex flex-col items-center mx-2">
-    <a href="/"><img src="{{ asset('assets/images/icon_coffee_tea_water.png') }}" class="w-[150px] h-[150px]"></a>
-    <x-card class="bg-slate-100 w-[450px] h-[450px] flex flex-col justify-center items-center">
+<x-layout :title="'Login'" class="flex flex-col items-center mx-2">  
+    <div class="w-full h-screen flex items-center">
+        <x-card class="bg-gradient-to-b from-slate-300 to-slate-100 w-[450px] h-[450px] flex flex-col justify-center items-center relative">
+                <img class="-top-[340px] absolute" src="{{ asset('assets/images/coffee_tea_water.png') }}" alt="">
+                <div class="flex justify-center w-full -right-3 -top-[50px] absolute">
+                    <span class="text-2xl text-white font-bold">C</span>
+                    <span class="text-2xl text-white font-bold">T</span>
+                    <span class="text-2xl text-white font-bold">W</span>
+                </div>
 
-        <x-message button_name="x" route="/clear-session" 
-                    message="{{ Session::get('message') }}" 
-                    class="bg-red-300 w-[360px] p-[10px] my-2"
-                    text_class="text-red-600 text-center" 
-                    link_btn_class="absolute -top-1 right-0 text-xl text-red-700 hover:text-red-800"
-                    />
-                    
-        <form class="w-full px-12" method="POST" action="{{ route('login') }}">
-            @csrf
-            <div class="flex flex-col gap-8 justify-center items-center">
-                <div class="w-full h-full flex flex-col gap-2 justify-center items-start">
-                    <label for="login" class="text-sm w-full">Username or Email</label>
-                    <input type="text" name="login" class="outline outline-1 rounded w-full h-[40px] px-2" value="{{old('login')}}"/>
-                    <x-inline-error-message element="login" />
+            <x-message button_name="x" route="/clear-session" 
+                        message="{{ Session::get('message') }}" 
+                        class="bg-red-300 w-[360px] p-[20px] my-5"
+                        text_class="text-red-600 text-center" 
+                        link_btn_class="absolute -top-1 right-0 text-xl text-red-700 hover:text-red-800"
+                        />
+                        
+            <form class="w-full px-12" method="POST" action="{{ route('login') }}">
+                @csrf
+                <div class="flex flex-col gap-8 justify-center items-center">
+                    <div class="w-full h-full flex flex-col justify-center items-start">
+                        <x-inline-error-message element="login" />
+                        <input type="text" name="login" class="rounded-lg w-full h-[60px] focus:outline-none shadow-md px-5 py-2" value="{{old('login')}}" placeholder="Username or Email"/>
+                        
+                    </div>
+                    <div class="w-full h-full flex flex-col gap-2 justify-center items-start">
+                        <x-inline-error-message element="password" />            
+                        <input type="password" name="password" class="rounded-lg w-full h-[60px] focus:outline-none shadow-md px-5 py-2" value="{{old('password')}}" placeholder="Password"/>
+                    </div>
+                    <div class="flex flex-col items-center gap-2 w-full">
+                        <x-button type="submit" name="Login" class="bg-black hover:bg-slate-800 text-white w-full h-[60px] rounded-lg shadow-lg font-bold"/>
+                        <x-link-button href="/register" name="Create an account?"/>
+                    </div>
                 </div>
-                <div class="w-full h-full flex flex-col gap-2 justify-center items-start">
-                    <label for="password" class="text-sm w-full">Password</label>
-                    <input type="password" name="password" class="outline outline-1 rounded w-full h-[40px] px-2"/>
-                    <x-inline-error-message element="password" />            
-                </div>
-                <div class="flex flex-col items-center gap-2">
-                    <x-button type="submit" severity="secondary" name="Login" class="w-[100px] h-[50px]"/>
-                    <x-link-button href="/register" name="Create an account?"></x-link-button> 
-                </div>
-            </div>
-        </form>
-    </x-card>
+            </form>
+        </x-card>
+    </div>
 </x-layout>
--- resources/views/pages/auth/register.blade.php
+++ resources/views/pages/auth/register.blade.php
@@ -1,42 +1,46 @@
 <x-layout :title="'Register'" class="flex flex-col items-center mx-2">
-    <a href="/"><img src="{{ asset('assets/images/icon_coffee_tea_water.png') }}" class="w-[150px] h-[150px]"></a>
-    <x-card class="bg-slate-100 w-[550px] h-auto min-h-[570px] flex justify-center items-center py-5">
+    <div class="w-full h-screen flex items-center">
+        <x-card class="bg-gradient-to-b from-slate-300 to-slate-100 w-[570px] h-[650px] flex flex-col justify-center items-center relative">
+                <img class="-top-[300px] h-[400px] absolute" src="{{ asset('assets/images/coffee_tea_water.png') }}" alt="">
+                <div class="flex justify-center w-full -right-2 -top-[50px] absolute">
+                    <span class="text-2xl text-white font-bold">C</span>
+                    <span class="text-2xl text-white font-bold">T</span>
+                    <span class="text-2xl text-white font-bold">W</span>
+                </div>
 
-        <x-message button_name="x" route="/clear-session" 
-                message="{{ Session::get('message') }}" 
-                class="bg-red-300 w-[360px] p-[10px]"
-                text_class="text-red-600 text-center" 
-                link_btn_class="absolute -top-1 right-0 text-xl text-red-700 hover:text-red-800"
-        />
-
-        <form class="w-full px-12" method="POST" action="{{ route('register') }}">
-            @csrf
-            <div class="flex flex-col gap-8 justify-center items-center">
-                <div class="w-full h-full flex flex-col gap-2 justify-center items-start">
-                    <label for="username" class="text-sm w-full">Username:</label>
-                    <input type="text" name="username" class="outline outline-1 rounded shadow-sm w-full h-[40px] px-2" value="{{old('username')}}">            
-                    <x-inline-error-message element="username" />
+            <x-message button_name="x" route="/clear-session" 
+                        message="{{ Session::get('message') }}" 
+                        class="bg-red-300 w-[360px] p-[20px] my-5"
+                        text_class="text-red-600 text-center" 
+                        link_btn_class="absolute -top-1 right-0 text-xl text-red-700 hover:text-red-800"
+                        />
+                        
+            <form class="w-full px-12 py-10" method="POST" action="{{ route('register') }}">
+                @csrf
+                <div class="flex flex-col gap-8 justify-center items-center">
+                    <div class="w-full h-full flex flex-col gap-2 justify-center items-start">
+                        <x-inline-error-message element="username" />
+                        <input type="text" name="username" class="rounded-lg w-full h-[60px] focus:outline-none shadow-md px-5 py-2" value="{{old('username')}}" placeholder="Username">            
+                    </div>
+                    <div class="w-full h-full flex flex-col justify-center items-start">
+                        <x-inline-error-message element="email" />
+                        <input type="text" name="email" class="rounded-lg w-full h-[60px] focus:outline-none shadow-md px-5 py-2" value="{{old('email')}}" placeholder="Email"/>
+                        
+                    </div>
+                    <div class="w-full h-full flex flex-col gap-2 justify-center items-start">
+                        <x-inline-error-message element="password" />            
+                        <input type="password" name="password" class="rounded-lg w-full h-[60px] focus:outline-none shadow-md px-5 py-2" placeholder="Password"/>
+                    </div>
+                    <div class="w-full h-full flex flex-col gap-2 justify-center items-start">
+                        <x-inline-error-message element="password" />            
+                        <input type="password" name="password_confirmation" class="rounded-lg w-full h-[60px] focus:outline-none shadow-md px-5 py-2" placeholder="Confirm Password"/>
+                    </div>
+                    <div class="flex flex-col items-center gap-2 w-full">
+                        <x-button type="submit" name="Register" class="bg-black hover:bg-slate-800 text-white w-full h-[60px] rounded-lg shadow-lg font-bold"/>
+                        <x-link-button href="/login" name="Already have an account?"/>
+                    </div>
                 </div>
-                <div class="w-full h-full flex flex-col gap-2 justify-center items-start">
-                    <label for="email" class="text-sm w-full">Email:</label>
-                    <input type="email" name="email" class="outline outline-1 rounded shadow-sm w-full h-[40px] px-2" value="{{old('email')}}">            
-                    <x-inline-error-message element="email" />
-                </div>
-                <div class="w-full h-full flex flex-col gap-2 justify-center items-start">
-                    <label for="password" class="text-sm w-full">Password:</label>
-                    <input type="password" name="password" class="outline outline-1 rounded shadow-sm w-full h-[40px] px-2">            
-                    <x-inline-error-message element="password" />
-                </div>
-                <div class="w-full h-full flex flex-col gap-2 justify-center items-start">
-                    <label for="password_confirmation" class="text-sm w-full">Confirm Password:</label>
-                    <input type="password" name="password_confirmation" class="outline outline-1 rounded shadow-sm w-full h-[40px] px-2">            
-                    <x-inline-error-message element="password" />
-                </div>
-                <div class="flex flex-col items-center">
-                    <x-button type="submit" severity="secondary" name="Register" class="w-[100px] h-[50px]"/>
-                    <x-link-button href="/login" name="Already have an account?"></x-link-button>
-                </div>
-            </div>
-        </form>
-    </x-card>
+            </form>
+        </x-card>
+    </div>
 </x-layout>
