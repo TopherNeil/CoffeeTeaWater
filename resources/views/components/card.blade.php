00e4cc000a1a82172fe80ea501ce3e701f4f3942
TopherNeil <gomez.cneil@gmail.com>
2025-01-18 14:53:14 +0800

    Removed default class attribute on card component

--- resources/views/components/card.blade.php
+++ resources/views/components/card.blade.php
@@ -1,3 +1,3 @@
-<div {{ $attributes->merge(['class' => 'rounded-lg border border-slate-400 shadow-md min-w-[300px] min-h-[300px]' ])}}>
+<div {{ $attributes->merge(['class' => 'rounded-lg shadow-md min-w-[300px] min-h-[300px]' ])}}>
     {{ $slot }}
 </div>
