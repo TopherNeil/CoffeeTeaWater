525f2f3bf438b6192bf59f067c1163b79474ab82
TopherNeil <gomez.cneil@gmail.com>
2025-01-26 15:30:04 +0800

    Added merging attributes for dynamic class

--- resources/views/components/icon-button.blade.php
+++ resources/views/components/icon-button.blade.php
@@ -1,7 +1,7 @@
 @props(['icon', 'active' => false, 'type' => 'submit', 'active_state' => '', 'inactive_state' => '', 'text' => null])
 
 
-<div role="button" class="group flex gap-2 shadow-md rounded-lg border border-slate-200 items-center px-4 py-2">
+<div role="button" {{$attributes->merge(["class" => "group flex gap-2 shadow-md rounded-lg border border-slate-200 items-center px-4 py-2"])}}>
     <x-dynamic-component :component="$icon" class="{{ $active ? $active_state : $inactive_state }} w-[25px] h-[25px]" />
     <span class="{{ $active ? $active_state : $inactive_state }} font-bold">{{ ucfirst($text) }}</span>
 </div>
