f977f5bc58646fc18681e0772676d0853dae6f2d
TopherNeil <gomez.cneil@gmail.com>
2025-01-27 20:12:24 +0800

    Added an option for button component

--- resources/views/components/button.blade.php
+++ resources/views/components/button.blade.php
@@ -1,4 +1,4 @@
-@props(['name', 'severity' => 'custom'])
+@props(['name', 'severity' => null, 'active' => false, 'active_state' => '', 'inactive_state' => ''])
 
 @switch($severity)
     @case('primary')
@@ -13,6 +13,11 @@
         @break
     @case('danger')
         <button {{ $attributes->merge(['class' => 'bg-gray-700 hover:bg-gray-800 text-white px-4 py-2 rounded-lg']) }}>
+            {{ $name }}
+        </button>
+        @break
+    @case('custom')
+        <button {{ $attributes->merge(['class' => ($active ? $active_state : $inactive_state) .  ' font-semibold px-4 py-2 rounded-lg bg-white shadow-md border border-slate-200']) }}>
             {{ $name }}
         </button>
         @break
