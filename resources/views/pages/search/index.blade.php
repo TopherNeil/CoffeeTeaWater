c7ffefcee1214f37e4f1ee736258d60e0983e25e
TopherNeil <gomez.cneil@gmail.com>
2025-02-05 22:15:42 +0800

    Feature:  AJAX Search

--- resources/views/pages/search/index.blade.php
+++ resources/views/pages/search/index.blade.php
@@ -1,4 +1,6 @@
 <x-layout>
     <x-navbar/>
-    Search works!
+    <x-card class="w-[530px] h-auto min-h-[300px] p-5 flex justify-center mt-4 rounded">
+        <livewire:search />
+    </x-card>
 </x-layout>
